a2b15448c0306f2d78345d5a712e3afe4ab74062
Glaiza Wagner <glaizawagner@gmail.com>
2019-10-07 09:55:14 -0700

    trelloyes

---
+++ src/list.js
@@ -0,0 +1,28 @@
+import React from 'react';
+import Card from './card'
+
+function List(props) {
+    const cardArr = props.cards.map(card => 
+        <Card title={card.title} content={card.content}/>
+    );
+
+    return (
+        <section class="List">
+            <header class="List-header">
+            <h2>{props.header}</h2>
+            </header>>
+
+            <div class="List-cards">
+                {cardArr}
+                <button type="button" class="List-add-button">
+                + Add Random Card
+                </button>
+            </div>
+
+
+        </section>
+    );
+}
+
+export default List;
+
